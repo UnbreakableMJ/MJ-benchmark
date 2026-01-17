ad16034567a802ac5e13a99e99f17a7c7586b39d
Mohamed Hammad <34196588+UnbreakableMJ@users.noreply.github.com>
2026-01-17 23:45:36 +0300

    Update platform.rs

--- mj-benchmark.rs/src/platform.rs
+++ mj-benchmark.rs/src/platform.rs
@@ -1,3 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+// MJ Benchmark
+// Copyright (c) 2024-2026
+// Mohamed Hammad
+//
+// Trademarks:
+//   Steelbore, S3cure, S3cure me, S3cure us, MJ Benchmark
+//   These names are trademarks of Mohamed Hammad and may not be used
+//   to endorse or promote derivative products without prior permission.
+
 use std::fmt;
 
 #[derive(Debug, Clone, Copy)]
